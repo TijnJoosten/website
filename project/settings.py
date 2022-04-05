290aa7f20641f0b336218c571e88ff9bd3bd93bb
Tijn <tijnjoosten@gmail.com>
2022-04-05 19:27:50 +0200

    Fixing secret key

--- project/settings.py
+++ project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-_h63_upptyu50b5oj0^%ln1i+m#uvpg9vfewhy$v*j-3!y_19n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,6 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_on_heroku
-django_on_heroku.settings(locals())
